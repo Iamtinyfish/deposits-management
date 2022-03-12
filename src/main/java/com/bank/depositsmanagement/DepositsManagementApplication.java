96cd37d9927d3c467679cdb6f755ccc27659ace4
Hieu Nguyen <hieunt.ptit.hp@gmail.com>
2022-03-12 21:55:15 +0700

    init project

---
+++ src/main/java/com/bank/depositsmanagement/DepositsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bank.depositsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/depositsmanagement/DepositsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.depositsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
