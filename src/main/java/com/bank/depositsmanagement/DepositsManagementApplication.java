4cab327901193d30b47748cca9b1fb1411964f90
Hieu Nguyen <hieunt.ptit.hp@gmail.com>
2022-05-09 17:54:22 +0700

    reconfig for heroku 3

--- src/main/java/com/bank/depositsmanagement/DepositsManagementApplication.java
+++ src/main/java/com/bank/depositsmanagement/DepositsManagementApplication.java
@@ -1,13 +1,7 @@
 package com.bank.depositsmanagement;
 
-import com.bank.depositsmanagement.config.InitData;
-import com.bank.depositsmanagement.dao.EmployeeRepository;
-import com.bank.depositsmanagement.dao.InterestRateReferenceRepository;
-import com.bank.depositsmanagement.dao.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DepositsManagementApplication {
